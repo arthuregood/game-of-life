b9bb77e4a0551f1d20f89e7d30909664eb687b13
arthuregood <arthuregood@protonmail.com>
2021-12-01 17:39:52 -0300

    change to boxplot

--- data_analysis.py
+++ data_analysis.py
@@ -7,15 +7,24 @@ import matplotlib.pyplot as plt
 
 # check if file exists
 file_exists = exists('life_analysis.csv')
+median = True
+savetype = 'total'
 
 if file_exists:
     df = pd.read_csv('life_analysis.csv')
-    df = df.groupby(np.arange(len(df))//10).mean()
+
+    if median:
+        df = df.groupby(np.arange(len(df))//10)
+        savetype = 'median'
 
     data = [('generations', 'Generations Analysis'),
             ('population_size', 'Population Analysis')]
 
     for y, title in data:
-        df.plot(kind='bar', x='probability', y=y, title=title)
+
+        df.boxplot('probability', y)
+        plt.xticks(range(0, 101, 10), range(0, 101, 10))
+        plt.title(title)
         plt.gcf().set_size_inches(19.2, 10.8)
         plt.savefig(fname=f'results/{y}')
+        plt.show()
